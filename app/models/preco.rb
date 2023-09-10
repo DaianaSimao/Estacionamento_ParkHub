2c65ead48e56847a96b7345a7f27172cf061701c
DaianaSimao <daiana.xd11@hotmail.com>
2023-09-10 12:40:54 -0300

    Criei um metodo que deixa a primeira letra do dado maiusculo.

--- app/models/preco.rb
+++ app/models/preco.rb
@@ -1,4 +1,11 @@
 class Preco < ApplicationRecord
   validates :tipo, presence: true
   validates :preco_hora, presence: true
+
+  before_save :capitalizar_nome
+
+  def capitalizar_nome
+    self.tipo = tipo.titleize if tipo.present?
+
+  end
 end

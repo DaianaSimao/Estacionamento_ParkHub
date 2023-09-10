d89765a4c6a1721db6e30769de82d1cc19215458
DaianaSimao <daiana.xd11@hotmail.com>
2023-09-10 12:39:45 -0300

    Criei um metodo que deixa maiuscula todas as letras do dados

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -2,8 +2,18 @@ class Checkin < ApplicationRecord
   belongs_to :preco
   belongs_to :vaga
   
+  
   after_create :atualizar_status_vaga
   after_update :atualizar_status_vaga
+  before_save :transformar_em_maiusculas
+
+  def transformar_em_maiusculas
+    self.veiculo_cor = veiculo_cor.upcase if veiculo_cor.present?
+    self.veiculo_placa = veiculo_placa.upcase if veiculo_placa.present?
+    self.veiculo_modelo = veiculo_modelo.upcase if veiculo_modelo.present?
+    self.veiculo_marca = veiculo_marca.upcase if veiculo_marca.present?
+    self.veiculo_placa = veiculo_placa.upcase if veiculo_placa.present?
+  end
 
   def calcular_preco
     hora_entrada = self.created_at

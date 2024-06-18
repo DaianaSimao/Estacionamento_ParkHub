b816fedc7b3636dff8e4864b45054b3a95beb79d
DaianaSimao <daiana.xd11@hotmail.com>
2024-06-18 09:52:32 -0300

    chore: Update render-build.sh script to include database migration step and start Rails server

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,23 @@ bundle install
 
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+# Função para verificar se o banco de dados está pronto
+check_db() {
+  echo "Waiting for the database to be ready..."
+  until pg_isready -h $DB_HOST -p 5432 -U $DB_USERNAME; do
+    sleep 1
+  done
+}
+
+# Verifique se o banco de dados está pronto antes de rodar a task
+check_db
+
+# Execute a task específica do Rails
+echo "Running specific Rails task..."
+bin/rails admin:criar_novo_admin
+bin/rails admin:criar_novo_funcionario
+
+# Inicie o servidor Rails
+echo "Starting Rails server..."
+bin/rails server -b 0.0.0.0

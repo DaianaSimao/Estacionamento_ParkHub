6e4615d284b9117def65134d96333edb30e450fe
DaianaSimao <daiana.xd11@hotmail.com>
2024-07-05 16:45:12 -0300

    chore: Refactor format_changes method in application_helper.rb for improved readability and performance

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def formatar_valor(valor)
     number_to_currency(valor, unit: 'R$', separator: ',', delimiter: '.', precision: 2)
   end
+
+  def format_changes(changes)
+    formatted_changes = ""
+    changes.each do |attribute, values|
+      old_value, new_value = values
+      formatted_changes += "<strong>#{attribute}:</strong> De <em>#{old_value}</em> para <em>#{new_value}</em><br>"
+    end
+    formatted_changes.html_safe
+  end
 end

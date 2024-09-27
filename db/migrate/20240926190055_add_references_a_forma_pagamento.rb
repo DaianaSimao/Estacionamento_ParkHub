5d7a34a2b12eeba02d0207abe00b5ab9bbc3b788
daiana.xd11@hotmail.com <daiana.xd11@hotmail.com>
2024-09-27 00:34:58 -0300

    Ajuste nas associações entre as tabelas forma pagamento, caixa e checkin.

--- db/migrate/20240926190055_add_references_a_forma_pagamento.rb
+++
@@ -1,5 +0,0 @@
-class AddReferencesAFormaPagamento < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :forma_pagamentos, :checkin, foreign_key: true
-  end
-end
--- db/migrate/20240926190236_add_references_a_checkin.rb
+++
@@ -1,5 +0,0 @@
-class AddReferencesACheckin < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :checkins, :caixa, foreign_key: true
-  end
-end

babc16c91318ef74fb2b051711d44f81ed1da188
daiana.xd11@hotmail.com <daiana.xd11@hotmail.com>
2024-09-30 16:21:13 -0300

    Refatorar views de tickets para atualizar telefone de contato

--- app/models/vaga.rb
+++ app/models/vaga.rb
@@ -1,7 +1,8 @@
 class Vaga < ApplicationRecord
   has_many :checkins
-  
   has_paper_trail
+
+  validates :status, inclusion: { in: [true, false] }
 
   def status_display
     status ? 'Ocupada' : 'Disponível'
